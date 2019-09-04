b8b7a83133d37c03929a7a57b7b52148ed2b5528
Todor Markov <TodorMarkov@abv.bg>
2019-09-04 15:23:19 +0300

    siteProject

---
+++ app.js
@@ -0,0 +1,42 @@
+jQuery(document).ready(function () {
+  $('a').click(function (event) {
+    header = $(this).attr('href')
+    $.ajax({
+      url: header,
+    })
+      .done(function (html) {
+        $('main').empty().append(html)
+      })
+      .fail(function () {
+        console.log('error')
+        return false
+      })
+      .always(function () {
+        console.log('complete')
+      })
+    return false
+  })
+})
+
+fetch("./main.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("main").innerHTML = data
+  })
+
+
+fetch("./footer.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("footer").innerHTML = data
+  })
+
+
+
+
+
+
